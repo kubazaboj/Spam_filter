15ff17086fd1bee68cac352c8a91872a003f5a57
spilavoj <spilavoj@fel.cvut.cz>
2022-12-30 16:47:17 +0100

    new method: train_mails() = generator, return spam_ham, mail spam_ham = "SPAM" / "HAM" podle souboru !truth.txt mail = text samotneho mailu

--- trainingcorpus.py
+++ trainingcorpus.py
@@ -1,11 +1,15 @@
 from corpus import Corpus
+import utils
 
 
 class TrainingCorpus(Corpus):
-    def get_train_emails(self):
+
+    def train_mails(self):
         file = "!truth.txt"
-        with open(self.dirname + "/" + file, encoding='utf-8') as f:
-            yield file, f.read()
+        truth_dir = utils.read_classification_from_file(self.dirname + "/" + file)
+        for name, mail in self.emails():
+            spam_ham = truth_dir[name]
+            yield spam_ham, mail
 
     def get_train_file_name(self):
         return self.dirname + "/" + "!truth.txt"
