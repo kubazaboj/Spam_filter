11008239681ea3866bce9d082ab4ae6394f70811
spilavoj <spilavoj@fel.cvut.cz>
2023-01-03 13:37:01 +0100

    pattern_counter classa

---
+++ pattern_counter.py
@@ -0,0 +1,15 @@
+class Pattern_counter:
+    def __init__(self):
+        self.char_counts = {}
+        self.caps_count = 0
+
+    # add word to pattern counter and check for all possible patterns
+    def add_word(self, word):
+        pass
+
+    # checks if word has capslock if yes adds to caps_count
+    def check_caps_lock(self, word):
+
+
+    # check word for charackters, adds to char_counts dictionary
+    def check_char(self, word, char):
